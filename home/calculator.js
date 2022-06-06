c60890eeae0fa264c18ec476a749badbb0baade8
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-06 16:56:16 -0500

    modified:   home/calculator.js 	modified:   home/index.html 	modified:   home/style.css

--- home/calculator.js
+++ home/calculator.js
@@ -1,11 +1,78 @@
+
+let formulaApp = document.querySelector('#formula');
+let res = 0;
+let calculate = [];
+
+let calc1 = "";
+let calcOp = 0;
+let calc2 = "";
+let appendForm = [];
+
+function kpBtnClick(e){
+    if (this.innerHTML == "=") {
+        const equation = formulaApp.innerHTML
+        console.log(equation);
+        calcOp = equation.split(/[0-9]/).join('');
+        calculate = Array.from(equation.split(/[^0-9]/));
+        
+        calc1 = parseInt(calculate.slice(0));
+        calc2 = parseInt(calculate.slice(1));
+        
+        console.log(calc1);
+        console.log(calcOp);
+        console.log(calc2);
+        
+        switch (calcOp) {
+            case "+":
+                res = addition(calc1,calc2);
+                document.querySelector('#result').innerHTML = String("= " + res);
+                console.log(res);
+                break;
+            case "-":
+                res = subtraction(calc1,calc2);
+                document.querySelector('#result').innerHTML = String("= " + res);
+                console.log(res);
+                break;
+            case "x":
+                res = multiplication(calc1,calc2);
+                document.querySelector('#result').innerHTML = String("= " + res);
+                console.log(res);
+                break;
+            case "/":
+                res = division(calc1,calc2).toFixed(5);
+                document.querySelector('#result').innerHTML = String("= " + res);
+                console.log(res);
+                break;
+        }
+        //console.log(parseInt(calculate[0]));
+
+        return;
+    }
+    
+    appendForm.push(this.innerHTML);
+    formulaApp.innerHTML = appendForm.join('');
+    //console.log(formulaApp.innerHTML);
+}
+
+
+const kpButtons = document.querySelectorAll('button');
+kpButtons.forEach(button => button.addEventListener('click', kpBtnClick));
+
 function addition(a,b) {
-    return (a+b);
+  return (a+b);
   };
   
 function subtraction(a,b) {
-    return (a-b);
+  return (a-b);
 };
   
+function multiplication(a,b) {
+  return (a*b);
+}
+
+function division(a,b){
+  return (a/b);
+}
 
   /*const sum = function(arr) {
     const sum = arr.reduce((total, a) => total + a, 0);
