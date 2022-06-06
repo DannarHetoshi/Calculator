e247bebfb10530eae6caa08857b44f6fc31b2594
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-06 01:52:00 -0500

    new file:   home/calculator.js 	new file:   home/index.html 	new file:   home/style.css

---
+++ home/calculator.js
@@ -0,0 +1,34 @@
+function addition(a,b) {
+    return (a+b);
+  };
+  
+function subtraction(a,b) {
+    return (a-b);
+};
+  
+
+  /*const sum = function(arr) {
+    const sum = arr.reduce((total, a) => total + a, 0);
+    return sum;
+  };*/
+  
+  /*const multiply = function(arr) {
+    const product = arr.reduce((total, a) => total*a, 1)
+    return product;
+  };*/
+  
+  /*const power = function(a,b) {
+    return (a**b);
+      
+  };*/
+  
+  /*const factorial = function(a) { 
+    let fact = a
+    if (fact < 2) {
+      return 1;
+    }
+    for (i = a; i>1; i--) {
+      fact *= (i-1);
+    }
+    return fact;
+  };*/
