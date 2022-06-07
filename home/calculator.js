dd070b82e5d36ebda0eeb4025826e0d3a2c5582f
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-07 04:19:50 -0500

    modified:   home/calculator.js
        improved calculator logic

--- home/calculator.js
+++ home/calculator.js
@@ -2,67 +2,131 @@
 let formulaApp = document.querySelector('#formula');
 let resApp = document.querySelector('#result');
 let appendForm = [];
-
+let appendRes = [];
+let res = 0;
+let calcOp = "";
 
 // capture button clicks for keypad, operators, clear button.
 // special logic for the "=" operator to perform the operation.
 // special logic for the "Clear" button to clear the fields.
 
-function kpBtnClick(e){
-    if (this.innerHTML == "=") {
-        performCalculation(e); //call the calculation function
-    } else if (this.innerHTML == "Clear") {
-      clearForm(); //call the clear form function
-      return;
-    }
+function clearForm(e) {
+  formulaApp.innerHTML = '';
+  appendForm = [];
+  resApp.innerHTML = '';
+}
+
+function opBtnClick(e){
+  if (this.innerHTML == "=") {
+//  console.log(calcOp);
+    performCalculation(e) //call the calculation function
+  } else if (resApp.innerHTML == "" && this.innerHTML != "=") {
+    calcOp = this.innerHTML;
     appendForm.push(this.innerHTML);
     formulaApp.innerHTML = appendForm.join('');
-    //console.log(formulaApp.innerHTML);
+  } else if (resApp.innerHTML != "" && this.innerHTML != "=") {
+    calcOp = this.innerHTML;
+    appendForm = appendRes;
+    appendRes.push(this.innerHTML);
+    formulaApp.innerHTML = (resApp.innerHTML + appendRes.join(''));
+  }
+}
+
+function kpBtnClick(e){
+
+  if (resApp.innerHTML == "" && this.innerHTML != "+/-") {
+    //console.log(this.innerHTML);
+    appendForm.push(this.innerHTML);
+    formulaApp.innerHTML = appendForm.join('');
+    //console.log(formulaApp.innerHTML); */
+  } else if (resApp.innerHTML != "" && this.innerHTML != "+/-") {
+    //console.log(appendForm);
+    //console.log(appendRes);
+    appendForm = appendRes;
+    appendRes.push(this.innerHTML);
+    formulaApp.innerHTML = (resApp.innerHTML + appendRes.join(''));
+
+  } else if (this.innerHTML == "+/-") {
+    if (formulaApp.innerHTML != "") {
+      appendForm = appendRes;
+      formulaApp.innerHTML = (resApp.innerHTML + appendRes.join(''));
+      let firstChar = formulaApp.innerHTML;
+    }
+    
+    let firstChar = formulaApp.innerHTML;
+    //console.log(firstChar);
+    //console.log(firstChar.charAt(0));
+    const arr = Array.from(firstChar);
+    switch (firstChar.charAt(0)){
+      default:
+        arr.splice(0,0, "-");
+        formulaApp.innerHTML = arr.join('');
+        //console.log(arr);
+        break;
+      case "-":
+        arr.splice(0,1);
+        formulaApp.innerHTML = arr.join('');
+        //console.log(arr);
+        break;
+    }
+  }
+}
+
+function prepareCalc() {
+  formulaApp.innerHTML = resApp.innerHTML.split(/[^0-9]+\s/).join('');
+  const calc1 = parseFloat(formulaApp.innerHTML);
+  //console.log(resApp.innerHTML);
+  //console.log(this);
+  appendForm.push(this.innerHTML);
+  formulaApp.innerHTML = appendForm.join('');
+  
 }
 
 function performCalculation() {
+
   const equation = formulaApp.innerHTML
-//console.log(equation);
-  const calcOp = equation.split(/[0-9$\s$]/).join('');
-  const calculate = Array.from(equation.split(/[^0-9]+\s/));
-  const calc1 = parseInt(calculate.slice(0));
-  const calc2 = parseInt(calculate.slice(1));
-  let res = 0;
-/*console.log(calc1);
-  console.log(calcOp);
-  console.log(calc2); */
+//  console.log(equation);
+  const calculate = Array.from(equation.split(/[+-]?[^\d]+\.?\s/));
+//  console.log(calculate);
+
+  const calc1 = parseFloat(calculate.slice(0));
+//  console.log(calc1)
+
+//  console.log(calcOp);
+
+  const calc2 = parseFloat(calculate.slice(1));
+//  console.log(calc2);
+
   
   switch (calcOp) { //switch for each operator
-      case "+":
+      case " + ":
           res = addition(calc1,calc2);
-          resApp.innerHTML = String("= " + res);
-//        console.log(res);
+          resApp.innerHTML = res;
+          appendRes = [];          
+          console.log(res);
           break;
-      case "-":
+      case " - ":
           res = subtraction(calc1,calc2);
-          resApp.innerHTML = String("= " + res);
-//        console.log(res);
+          resApp.innerHTML = res;
+          appendRes = [];
+          console.log(res);
           break;
-      case "*":
+      case " * ":
           res = multiplication(calc1,calc2);
-          resApp.innerHTML = String("= " + res);
-//        console.log(res);
+          resApp.innerHTML = res;
+          appendRes = [];
+          console.log(res);
           break;
-      case "/":
+      case " / ":
           res = division(calc1,calc2).toFixed(5);
-          resApp.innerHTML = String("= " + res);
-//        console.log(res);
+          resApp.innerHTML = res;
+          appendRes = [];
+          console.log(res);
           break;
   }
   //console.log(parseInt(calculate[0]));
 
   return;
-}
-
-function clearForm() {
-  formulaApp.innerHTML = '';
-  appendForm = [];
-  resApp.innerHTML = '';
 }
 
 function addition(a,b) {
@@ -81,8 +145,12 @@ function division(a,b){
   return (a/b);
 }
 
-const kpButtons = document.querySelectorAll('button');
+const kpButtons = document.querySelectorAll('button.kpBtn');
 kpButtons.forEach(button => button.addEventListener('click', kpBtnClick));
+const opButtons = document.querySelectorAll('button.opBtn')
+opButtons.forEach(button => button.addEventListener('click', opBtnClick));
+const clrBtn = document.querySelector('button.clearBtn');
+clrBtn.addEventListener('click', clearForm);
 
 
   /*const sum = function(arr) {
